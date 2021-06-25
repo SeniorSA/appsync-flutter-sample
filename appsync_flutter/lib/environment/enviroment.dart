46acbc5b6647d494b10bd81d1166cca9c07082df
jorgekg <jorgekg3@gmail.com>
2021-06-25 11:07:06 -0300

    Ambiente

--- appsync_flutter/lib/environment/enviroment.dart
+++ appsync_flutter/lib/environment/enviroment.dart
@@ -1,4 +1,4 @@
 class Environment {
-  static final String graphQLEndpoint = "https://vbebv3tlqrekhb3dvd6ju4c3vm.appsync-api.us-east-1.amazonaws.com/graphql";
-  static final String graphQLEndApiKey = "da2-fpcd5kfm7bbjpajwojmgyphade";
+  static final String graphQLEndpoint = "endpoint/graphql";
+  static final String graphQLEndApiKey = "api_key";
 }
